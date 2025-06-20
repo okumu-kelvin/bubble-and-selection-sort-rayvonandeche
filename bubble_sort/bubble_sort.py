230d371e378e09112b9fc708ec645ce946fa1af6
Rayvon <158500435+rayvonandeche@users.noreply.github.com>
2025-06-20 07:22:27 +0000

    .

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,9 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+
+    for i in range(n-1):
+        for j in range(n-1):
+            if unsorted_list[j] > unsorted_list[j+1]:
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+
+    return unsorted_list
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
+def min(arr, i, n):
+    smallest = arr[i]
+    pos = i
+    for j in range(i + 1, n):
+        if arr[j] < smallest:
+            smallest = arr[j]
+            pos = j
+            
+    return pos
+
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for i in range(n - 1):
+        pos = min(arr, i, n)
+        if i != pos:
+            arr[i], arr[pos] = arr[pos], arr[i]
+            
+    return arr
